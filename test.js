4ba72fc16064a1eaae61aa2396bb7b4faf289ebe
Jan Richter <rich2@seznam.cz>
2020-04-27 13:57:20 +0200

    fix selector

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ describe('Paid Notify Extension tests', () => {
     it('Paid article notification is shown', async () => {
         await page.goto('https://www.firstclass.cz/2020/04/proc-zeny-potrebuji-oporu-a-muzi-pochopit-archimedia/', {waitUntil: 'networkidle2'});
         // await expect(page).toMatch('googleaaa');
-        const extensionBar = await page.waitForSelector('.ext-paid-notifya', {
+        const extensionBar = await page.waitForSelector('.ext-paid-notify', {
             timeout: 2000,
             visible: true
         });
