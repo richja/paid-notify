f4386d4dcc5b16686d4f2b877d52ce12056af894
Jan Richter <rich2@seznam.cz>
2020-05-11 15:48:01 +0200

    basic test one sample page

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,8 +5,16 @@ module.exports = {
         args: [
             '--disable-extensions-except=C:/xampp/htdocs/projects/paid_notify/dist',
             '--load-extension=C:/xampp/htdocs/projects/paid_notify/dist',
+            '--disable-features=AudioServiceOutOfProcess',
+            '--no-first-run',
+            '--disable-gpu',
+            '--disable-software-rasterizer',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--no-zygote',
         ],
-        devtools: false
+        devtools: false,
     },
     // browser: 'chrome',
     browserContext: 'default',
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 describe('Paid Notify Extension tests', () => {
     it('Paid article notification is shown', async () => {
-        await page.goto('https://www.firstclass.cz/2020/04/proc-zeny-potrebuji-oporu-a-muzi-pochopit-archimedia/', {waitUntil: 'networkidle2'});
-        // await expect(page).toMatch('googleaaa');
-        const extensionBar = await page.waitForSelector('.ext-paid-notify', {
-            timeout: 2000,
-            visible: true
-        });
+        const response = await page.goto('https://denikn.cz/354496/babisem-kritizovany-namestek-dostal-po-rezignaci-pres-milion-aby-neodesel-ke-konkurenci-za-sedm-tydnu-se-na-urad-vratil/?ref=tit', {waitUntil: 'networkidle2'});
+        expect(response.ok()).toBeTruthy();
+
+        try {
+            const extensionBar = await page.waitForSelector('.ext-paid-notify', {
+                timeout: 2000,
+                visible: true
+            });
+        }
+        catch (e) {
+            console.log(e);
+            fail("Extension notification was not rendered");
+        }
     })
 })
